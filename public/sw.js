78080221d704341d2f35292afd724c4b616103dc
RB\binoy.sinha <binoy.sinha@redbus.com>
2017-11-07 11:47:10 +0530

    first commit

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Preccahing app shell...');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',          
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          'src/images/main-image.jpg'
+        ]);        
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response){
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
